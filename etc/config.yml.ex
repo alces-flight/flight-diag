65662668b94ffbaf075d93ae483ccc170d194e7f
Mark J. Titorenko <mark.titorenko@alces-flight.com>
2019-07-12 14:56:26 +0100

    Add (configurable, optional) remote access capabilities.

--- etc/config.yml.ex
+++ etc/config.yml.ex
@@ -1,3 +1,14 @@
 ---
-# path where flight-diag.log should be written for privileged access logs
-log_filepath: /var/log
+# path where flight-diag.log should be written for privileged access
+# logs (defaults to var/log in diag installation directory)
+#log_filepath: /var/log
+# enable remote access features (defaults to false)
+#enable_remote: false
+# path to diag binary on remote host (defaults to same path as client system)
+#remote_diag_path: /opt/flight-diag/bin/diag
+# override user for access to diag on remote host (defaults to SUDO_USER)
+#remote_diag_user: siteadmin
+# access remote host from local root account (defaults to SUDO_USER account)
+#remote_from_root: false
+# domain to append to node names (defaults to domain name of client system)
+#domain_name: pri.mycluster.alces.network
